b5d172e2b347c37cd29a32a3a92b583678915a54
Jayin Ton <tonjayin@gmail.com>
2017-02-18 01:23:43 +0800

    more declear

--- Model/CronModel.class.php
+++ Model/CronModel.class.php
@@ -13,9 +13,9 @@ class CronModel extends Model {
 	//自动验证
 	protected $_validate = array(
 		//array(验证字段,验证规则,错误提示,验证条件,附加规则,验证时间)
-		array('subject', 'require', '计划任务名称不能为空！', 1, 'regex', 3),
-		array('loop_type', 'require', '计划任务类型不能为空！', 1, 'regex', 3),
-		array('cron_file', 'require', '计划任务执行文件不能为空！', 1, 'regex', 3),
+		array('subject', 'require', '计划任务名称不能为空！', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
+		array('loop_type', 'require', '计划任务类型不能为空！', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
+		array('cron_file', 'require', '计划任务执行文件不能为空！', self::MUST_VALIDATE, 'regex', self::MODEL_BOTH),
 	);
 	//自动完成
 	protected $_auto = array(
