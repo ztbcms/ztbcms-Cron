a9672ff20cd393794af131bb4d4c67f437d2ecc6
Jayin Ton <tonjayin@gmail.com>
2017-05-13 14:58:36 +0800

    Demo计划任务模拟异常退出

--- CronScript/Demo.class.php
+++ CronScript/Demo.class.php
@@ -7,6 +7,7 @@
 namespace Cron\CronScript;
 
 use Cron\Base\Cron;
+use Think\Exception;
 
 class Demo extends Cron {
 
@@ -14,7 +15,10 @@ class Demo extends Cron {
 	public function run($cronId) {
 		\Think\Log::record("我执行了计划任务事例 Demo.class.php！");
 		echo 'this is demo cron';
+		//模拟长时间执行
 		sleep(2);
+		//模拟出错了
+        throw new Exception('突然出错了');
 	}
 
 }
