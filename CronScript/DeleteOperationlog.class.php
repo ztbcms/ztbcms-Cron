cd904a0b5048aca826b33ca5e338b1c3d50f2daf
Jayin Ton <tonjayin@gmail.com>
2018-05-28 15:15:51 +0800

    Refactor

--- CronScript/DeleteOperationlog.class.php
+++ CronScript/DeleteOperationlog.class.php
@@ -9,7 +9,7 @@ namespace Cron\CronScript;
 use Cron\Base\Cron;
 
 /**
- * 删除计划任务日志(默认删除30日前的日志)
+ * 删除管理后台的日志(默认删除60日前的日志)
  *
  * 建议每日执行一次
  */
@@ -21,10 +21,10 @@ class Deleteoperationlog extends Cron {
      * @param string $cronId
      */
     public function run($cronId) {
-        \Think\Log::record("Run:DeleteCronLog");
-        $time = time()- 60*60*24*60; //30日前
-        $where['time'] = array('ELT',$time);
+        \Think\Log::record("Run:Deleteoperationlog");
+        $time = time() - 60 * 60 * 24 * 60;
+        $where['time'] = array('ELT', $time);
         $res = D('Cron/Operationlog')->where($where)->delete();
-        echo '删除计划任务日志记录数:' . $res;
+        echo '删除日志记录数:' . $res;
     }
 }
